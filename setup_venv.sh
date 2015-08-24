5c93d44fdd247450bd8a4c0825cb06b1964fffa3
Marcin Biernat <biern.m@gmail.com>
2015-08-24 16:55:24 +0200

    venv3 setup

--- setup_venv.sh
+++ setup_venv.sh
@@ -4,3 +4,10 @@ virtualenv $VENV --prompt="(emacs))"
 source $VENV/bin/activate
 pip install jedi==0.8.1-final0
 pip install flake8
+
+VENV3=~/.emacs.d/venv3
+
+pyvenv $VENV3
+source $VENV3/bin/activate
+pip install jedi==0.8.1-final0
+pip install flake8
